f46bb68e609c2742d86a0d5f49f625ceccdcd706
Matilde Santos Pereira <matilde.pereira@mail.com>
2022-07-07 14:21:51 +0100

    Initial Commit

---
+++ Learning GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning GH
+//
+//  Created by Matilde Pereira on 07/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Matilde Pereira on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
